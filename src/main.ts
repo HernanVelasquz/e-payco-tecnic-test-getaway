65050ccadb40c59f48a3c893d49ebc1df8d8210f
Hernan Velasquez Meriño <velasquezmhernan@gmail.com>
2024-10-24 01:36:49 -0500

    feat: modificando la implementacion del motor de compilacion y cambiando la plataforma de configuracion de express a fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
+import { AppModule } from './app.module';
+import { envs } from './config';
 async function main() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true}),
+  );
+
+  app.setGlobalPrefix('epayco-backing-api');
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(envs.port);
 }
 main();
